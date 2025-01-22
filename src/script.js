3a369ee6988a5354978f157612a6fe1d30dc15de
humbertoragazzini <beto_vicio@hotmail.com>
2025-01-22 07:38:23 +0000

    added utime to the displacement shader

--- src/script.js
+++ src/script.js
@@ -239,6 +239,7 @@ effectComposer.addPass(tintPass);
 const DisplacementPassShader = {
     uniforms: {
         tDiffuse: { value: null },
+        uTime: { value: null },
     },
     vertexShader: `
         varying vec2 vUv;
@@ -249,10 +250,13 @@ const DisplacementPassShader = {
     `,
     fragmentShader: `
         uniform sampler2D tDiffuse;
+        uniform float uTime;
         varying vec2 vUv;
         void main(){
-            vec2 newUv = vUv;
-            newUv.y += 0.2;
+            vec2 newUv = vec2(
+            vUv.x,
+            vUv.y + sin(vUv.x * uTime) * 0.1
+            );
             vec4 color = texture2D(tDiffuse, newUv);
             gl_FragColor = color;
         }
