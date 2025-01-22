e44fb1b5b3dc7f60f36f29f76ce93e354ddf82c9
humbertoragazzini <beto_vicio@hotmail.com>
2025-01-22 08:33:58 +0000

    finished helmet displacement

--- src/script.js
+++ src/script.js
@@ -273,6 +273,7 @@ effectComposer.addPass(drunkDisplacementPass);
 const VisorDisplacementPassShader = {
     uniforms: {
         tDiffuse: { value: null },
+        uNormalMap: { value: null },
     },
     vertexShader: `
         varying vec2 vUv;
@@ -283,17 +284,25 @@ const VisorDisplacementPassShader = {
     `,
     fragmentShader: `
         uniform sampler2D tDiffuse;
+        uniform sampler2D uNormalMap;
         uniform float uTime;
         varying vec2 vUv;
         void main(){
-            vec2 newUv = vUv;
+            vec3 normalColor = texture2D(uNormalMap, vUv).xyz * 2.0 - 1.0;
+            vec2 newUv = vUv + normalColor.xy * 0.1;
             vec4 color = texture2D(tDiffuse, newUv);
+            vec3 lightDirection = normalize(vec3(-1.0,1.0,0.0));
+            float lightness = clamp(dot(normalColor, lightDirection),0.0,1.0);
+            color.rgb += lightness * 2.0;
             gl_FragColor = color;
         }
     `,
 };
 
 const visorDisplacementPass = new ShaderPass(VisorDisplacementPassShader);
+visorDisplacementPass.material.uniforms.uNormalMap.value = textureLoader.load(
+    "/textures/interfaceNormalMap.png"
+);
 effectComposer.addPass(visorDisplacementPass);
 
 // This is the gamma correction pass, because we are using effectComposer the color need to be converted to lineal, the gamma correction pass make this with a custom shader
